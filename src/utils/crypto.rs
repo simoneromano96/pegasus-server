7392fc90cd2488ab3e37224b60b0f4baf7e738bc
Simono il magnifico <simoneromano@tutanota.de>
2021-03-28 13:06:46 +0200

    hashed key to enable variable length passwords

--- src/utils/crypto.rs
+++ src/utils/crypto.rs
@@ -5,23 +5,38 @@ use chacha20poly1305::{
 use log::debug;
 use rand::{Rng, RngCore, SeedableRng};
 use rand_chacha::ChaCha20Rng;
+use sha3::{Digest, Sha3_256};
 
 pub fn encrypt_data(key: String, data: String) -> Vec<u8> {
   debug!("Encrypting: {:?} with {:?}", &data, &key);
 
-  // MUST be 32-bytes
-  let key = Key::from_slice(key.as_bytes());
+  // Create a SHA3-256 object
+  let mut hasher = Sha3_256::new();
 
+  // Write input message
+  hasher.update(key.as_bytes());
+
+  // Read hash digest
+  let hashed_key = hasher.finalize();
+
+  debug!("{:?}", &hashed_key);
+
+  // The key MUST be 32-bytes or 256-bits, this is why we hash it
+  let key = Key::from_slice(&hashed_key);
+
+  // Create the cipher with the given key
   let cipher = XChaCha20Poly1305::new(key);
 
+  // Generate a random nonce getting entropy from the OS
   let mut rng = ChaCha20Rng::from_entropy();
   let random_nonce: [u8; 24] = rng.gen();
 
   debug!("{:?}", &random_nonce);
 
-   // MUST be 24-bytes; unique
+  // The MUST be 24-bytes or 192-bits and unique
   let nonce = XNonce::from_slice(&random_nonce);
 
+  // Finally encrypt the text
   let ciphertext = cipher
     .encrypt(nonce, data.as_bytes())
     .expect("encryption failure!");
